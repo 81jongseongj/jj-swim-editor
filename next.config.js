ffc699b7cb1821b2942e74b7d6a5dd7e9f6c0b7d
jongseongj <jongseongj@gmail.com>
2025-04-17 23:15:18 +0900

    fix: force add new.tsx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx']
+};
+
+module.exports = nextConfig;
